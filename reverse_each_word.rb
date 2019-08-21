c26bdb88669a0fed310ab434e321083529e4fa8b
Dylan Crawshaw <dcrawsh@gmail.com>
2019-08-21 03:26:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 sentence="i like to eat lots of cake"
 
 def reverse_each_word(sentence)
-  sentence.split.map {|word| word.reverse}.join(" ")
+  sentence.split.map do |word| 
+  word.reverse
+  word.join(" ")
 end
 
 puts reverse_each_word(sentence)
